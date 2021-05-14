fe52f89b9a22521f325a2daa9922b73bd5a8d009
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-13 19:12:25 -0600

    added fetch request in useEffect -- app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+// imports
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import React from "react";
 
 function App() {
+  // setting up a stateful employees array 
+  const [employees, setEmployees] = useState([])
+  // useEffect to replace componentDidMount
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=40")
+    .then((response) => response.json)
+    .then((data) => {
+      setEmployees(data.results)
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      Employee Directory
       </header>
     </div>
   );
